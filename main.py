6a855e7039eda0884b264ff5a78d929b5232c10b
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-10-24 22:12:01 +0200

    Better names

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ CSV_HEADER = ["date", "famille_transport", "ligne", "regularite"]
 dates = set()
 data = []
 
-for file in glob.glob("data/*.html"):
-    print(f"Processing {file}")
-    with open(file) as f:
+for filename in glob.glob("data/*.html"):
+    print(f"Processing {filename}")
+    with open(filename) as f:
         html_content = f.read()
     parsed = Parser(html_content)
     if parsed.date() in dates:
@@ -17,7 +17,7 @@ for file in glob.glob("data/*.html"):
     dates.add(parsed.date())
     data.extend(parsed.to_list())
 
-with open("regularite.csv", "w") as f:
+with open("data/regularite.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerow(CSV_HEADER)
     writer.writerows(data)
